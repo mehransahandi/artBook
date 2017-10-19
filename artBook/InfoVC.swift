049659c24f9462eb6ab1701b5568f813c59e3b73
mehran sahandi far <mehran.sahandi@yahoo.com>
2017-10-19 15:48:44 +0300

    design process

---
+++ artBook/InfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  InfoVC.swift
+//  artBook
+//
+//  Created by Mehran Sahandi on 19.10.2017.
+//  Copyright © 2017 Mehran Sahandi. All rights reserved.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var TXTArtName: UITextField!
+    @IBOutlet weak var TXTArtistName: UITextField!
+    @IBOutlet weak var TXTYear: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func BTNSavePressed(_ sender: Any) {
+        
+        
+    }
+    
+
+}
--- artBook/MainVC.swift
+++ artBook/MainVC.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func BTNAddPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "ToInfoVC", sender: nil)
+        
     }
-
+    
 
 }
 
